3f788ebd89645ee681c5bb2f2aa98b12a21b5d3d
DJ-SILKE <64040187+DJ-SILKE@users.noreply.github.com>
2021-03-19 21:34:35 +0100

    Add files via upload

---
+++ installer-v3.py
@@ -0,0 +1,75 @@
+import os,time,random,sys,time,colorama
+from time import sleep
+from colorama import Fore, Back, Style
+colorama.init()
+os.system("cls")
+os.system("title DDOS INSTALLER")
+
+name ="admin"
+password ="yes"
+print("Simple DDOS installer! by DJ_SILKE")
+for i in range(900000000000000000000000):
+	pwd = input("Install? (\u001b[32mYes\u001b[37m/\u001b[31mNo\u001b[37m) : ")
+	j=1
+	if(pwd==password):
+    	
+		break
+
+
+	else:
+		print("Use Ctrl + c to exit!")
+		continue
+
+os.system("cls")
+while True:
+    
+	print("\u001b[40;1m \u001b[32m")
+	print("8 888888888o.      8 888888888o.          ,o888888o.        d888888o.  ")
+	time.sleep(0.1)
+	print("8 8888    `^888.   8 8888    `^888.    . 8888     `88.    .`8888:' `88.")
+	time.sleep(0.1)
+	print("8 8888        `88. 8 8888        `88. ,8 8888       `8b   8.`8888.   Y8")
+	time.sleep(0.1)
+	print("8 8888         `88 8 8888         `88 88 8888        `8b  `8.`8888.    ")
+	time.sleep(0.1)
+	print("8 8888          88 8 8888          88 88 8888         88   `8.`8888.   ")
+	time.sleep(0.1)
+	print("8 8888          88 8 8888          88 88 8888         88    `8.`8888.  ")
+	time.sleep(0.1)
+	print("8 8888         ,88 8 8888         ,88 88 8888        ,8P     `8.`8888. ")
+	time.sleep(0.1)
+	print("8 8888        ,88' 8 8888        ,88' `8 8888       ,8P  8b   `8.`8888.")
+	time.sleep(0.1)
+	print("8 8888    ,o88P'   8 8888    ,o88P'    ` 8888     ,88'   `8b.  ;8.`8888")
+	time.sleep(0.1)
+	print("8 888888888P'      8 888888888P'          `8888888P'      `Y8888P ,88P'")
+	print("\u001b[0m_\u001b[37m ")
+	print("More info at https://github.com/DJ-SILKE/DDOS_INSTALLER")
+	print("\u001b[31mDisclaimer: education purpose only!\u001b[37m")
+	print("_")
+	break
+
+
+name ="admin"
+password2 ="A"
+
+
+nameuser = input("user name of the pc : ")
+
+for i in range(900000000000000000000000):
+	pwd2 = input("\u001b[32m(A)gree \u001b[31m(D)eny\u001b[37m : " )
+	j=1
+	if(pwd2==password2):
+    	
+		break
+    	
+	else:
+		print("you must Agree! to install the sofware")
+		continue
+
+os.system("cls")
+
+print("You must have git installed!")
+os.system("mkdir DDOS")
+os.system("cd DDOS")
+os.system("git clone https://github.com/DJ-SILKE/DDOS.exe-git-clone.git")
